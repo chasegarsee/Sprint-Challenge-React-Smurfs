18dc12fb648df66e4523a65d4c2281b92bb0b203
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-04-05 10:59:12 -0500

    Add Smurf functionality (axios.post)

--- village/src/App.js
+++ village/src/App.js
@@ -1,23 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import './App.css';
-import SmurfForm from './components/SmurfForm';
-import Smurfs from './components/Smurfs';
+import "./App.css";
+import SmurfForm from "./components/SmurfForm";
+import Smurfs from "./components/Smurfs";
+
+import axios from "axios";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      smurfs: [],
+      smurfs: []
     };
   }
+
+  componentDidMount() {
+    axios("http://localhost:3333/smurfs")
+      .then(res => this.setState({ smurfs: res.data }))
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  handleUpdateSmirfs = smurfs => {
+    this.setState({
+      smurfs
+    });
+  };
+
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
   render() {
     return (
       <div className="App">
-        <SmurfForm />
+        <SmurfForm updateSmirfs={this.handleUpdateSmirfs} />
         <Smurfs smurfs={this.state.smurfs} />
       </div>
     );
